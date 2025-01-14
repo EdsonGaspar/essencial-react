ce811d56128283f0bff81d52ba4a7cce49ded008
Edson João Gaspar <edsonjoaogaspar21@gmail.com>
2025-01-14 17:55:47 +0100

    Fix: Ajuste no tamanho da aplicação

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,9 @@
 export function Rodape() {
   return (
-    <footer className="bg-zinc-700 text-zinc-200 text-sm text-center py-4">
-      <h4>Todos os direitos reservados &copy; {new Date().getFullYear()}</h4>
+    <footer className="bg-zinc-700 ">
+      <div className="max-w-7xl w-full m-auto text-zinc-200 text-sm text-center py-4">
+        <h4>Todos os direitos reservados &copy; {new Date().getFullYear()}</h4>
+      </div>
     </footer>
   );
 }
--- src/components/template/pagina.tsx
+++ src/components/template/pagina.tsx
@@ -6,9 +6,9 @@ import { Cabecalho } from "../header";
 export default function Pagina(props: any) {
   // console.log(props.children);
   return (
-    <main className="flex flex-col w-screen h-screen">
+    <main className="flex flex-col w-screen h-screen bg-zinc-700">
       <Cabecalho />
-      <section className="flex-1 flex bg-zinc-500">
+      <section className="max-w-7xl w-full m-auto flex-1 flex bg-zinc-500 rounded-md overflow-hidden">
         <MenuLateral />
         <ConteudoMenu>{props.children}</ConteudoMenu>
       </section>
